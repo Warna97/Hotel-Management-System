12799850d1ce4b352e96cfce6c63250cb4a6b99e
H W M Lahiru Warna Bandara Senevirathne <76489840+Warna97@users.noreply.github.com>
2024-08-10 01:03:20 +0530

    Initial commit

---
+++ backend/hotel-management-system/src/test/java/com/example/hotelmanagement/hotel_management_system/HotelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelmanagement.hotel_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
